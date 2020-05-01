ac6b4144070d7fd21a6fc6cd94cd02cb3907b7e7
danarn17 <mdaenar@gmail.com>
2020-05-02 00:48:29 +0700

    upload project

---
+++ src/components/BannerShayna.vue
@@ -0,0 +1,129 @@
+<template>
+  <!-- Women Banner Section Begin -->
+  <section class="women-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5">
+          <carousel :items="3" :nav="false" :dots="false" :autoplay="true">
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/c-gatot.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view">
+                    <a href="product.html">+ Quick View</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Tas Belacu</div>
+                <a href="#">
+                  <h5>Mahyang Gatotkaca</h5>
+                </a>
+                <div class="product-price">
+                  $14.00
+                  <span>$35.00</span>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/w-nakula.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Bags</div>
+                <a href="#">
+                  <h5>Mahyang Nakula</h5>
+                </a>
+                <div class="product-price">
+                  $13.00
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/c-arjuna.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Bags</div>
+                <a href="#">
+                  <h5>Mahyang Arjuna</h5>
+                </a>
+                <div class="product-price">
+                  $13.00
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/w-puntadewa.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Bags</div>
+                <a href="#">
+                  <h5>Mahyang Puntadewa</h5>
+                </a>
+                <div class="product-price">
+                  $13.00
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/c-werkudara.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Bags</div>
+                <a href="#">
+                  <h5>Mahyang Werkudara</h5>
+                </a>
+                <div class="product-price">
+                  $13.00
+                </div>
+              </div>
+            </div>
+          </carousel>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Women Banner Section End -->
+</template>
+<script>
+import carousel from "vue-owl-carousel";
+export default {
+  name: "BannerShayna",
+  components: { carousel },
+};
+</script>
+<style scoped>
+.product-item {
+  margin-right: 25px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <SliderShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import SliderShayna from "@/components/SliderShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    SliderShayna,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
+</script>
