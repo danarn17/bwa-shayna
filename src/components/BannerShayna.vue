3117d3e83c61beb8482f6f901eb4eebedc795e8c
danarn17 <mdaenar@gmail.com>
2020-05-10 00:15:42 +0700

    ambil data produk dari API

--- src/components/BannerShayna.vue
+++ src/components/BannerShayna.vue
@@ -3,14 +3,23 @@
   <section class="women-banner spad">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-12 mt-5">
+        <div class="col-lg-12 mt-5" v-if="products.length>0">
           <carousel :items="3" :nav="false" :dots="false" :autoplay="true">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/c-gatot.jpg" alt="" />
+            <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
+              <div
+                class="pi-pic"
+                v-for="fotoProduk in itemProduct.galleries"
+                v-bind:key="fotoProduk.id"
+              >
+                <div v-if="fotoProduk.is_default==1">
+                  <img v-bind:src="fotoProduk.photo" alt />
+                </div>
+
                 <ul>
                   <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
+                    <a href="#">
+                      <i class="icon_bag_alt"></i>
+                    </a>
                   </li>
                   <li class="quick-view">
                     <a href="product.html">+ Quick View</a>
@@ -18,97 +27,21 @@
                 </ul>
               </div>
               <div class="pi-text">
-                <div class="catagory-name">Tas Belacu</div>
+                <div class="catagory-name">{{ itemProduct.type }}</div>
                 <a href="#">
-                  <h5>Mahyang Gatotkaca</h5>
+                  <h5>{{ itemProduct.name }}</h5>
                 </a>
                 <div class="product-price">
-                  $14.00
+                  Rp. {{ itemProduct.price }}
                   <span>$35.00</span>
                 </div>
               </div>
             </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/w-nakula.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Bags</div>
-                <a href="#">
-                  <h5>Mahyang Nakula</h5>
-                </a>
-                <div class="product-price">
-                  $13.00
-                </div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/c-arjuna.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Bags</div>
-                <a href="#">
-                  <h5>Mahyang Arjuna</h5>
-                </a>
-                <div class="product-price">
-                  $13.00
-                </div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/w-puntadewa.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Bags</div>
-                <a href="#">
-                  <h5>Mahyang Puntadewa</h5>
-                </a>
-                <div class="product-price">
-                  $13.00
-                </div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/c-werkudara.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Bags</div>
-                <a href="#">
-                  <h5>Mahyang Werkudara</h5>
-                </a>
-                <div class="product-price">
-                  $13.00
-                </div>
-              </div>
-            </div>
           </carousel>
+        </div>
+
+        <div class="col-lg-12" v-else>
+          <p>Tidak ada product baru untuk saat ini.</p>
         </div>
       </div>
     </div>
@@ -117,9 +50,21 @@
 </template>
 <script>
 import carousel from "vue-owl-carousel";
+import axios from "axios";
 export default {
   name: "BannerShayna",
   components: { carousel },
+  data() {
+    return {
+      products: []
+    };
+  },
+  mounted() {
+    axios
+      .get("http://shayna-backend.test/api/products")
+      .then(res => (this.products = res.data.data.data))
+      .catch(err => console.log(err));
+  }
 };
 </script>
 <style scoped>
